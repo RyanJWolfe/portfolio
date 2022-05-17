9738dad97a83647d454ac9e25b0182a7d7639fa2
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-05-17 04:08:19 -0700

    Hide header if not on home section, slightly reduce experience section sizing

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["navbar"]
+  static targets = ["navbar", "homeSection"]
 
   onKeyup(e) {
     if (e.key === "ArrowUp") {
@@ -20,11 +20,41 @@ export default class extends Controller {
     }
   }
 
+  onHover() {
+    clearTimeout(this.timer)
+    this.keepShowingNavbar()
+
+  }
+
   showNavbar() {
     this.navbarTarget.style.top = "0"
+    clearTimeout(this.timer)
+    this.keepShowingNavbar()
   }
 
   hideNavbar() {
     this.navbarTarget.style.top = "-150px"
+  }
+
+  keepShowingNavbar() {
+    this.timer = setTimeout(() => {
+      if (this.isScrolledIntoView(this.homeSectionTarget) === false) {
+        this.hideNavbar()
+      } else {
+        this.showNavbar()
+      }
+    }, 2000)
+  }
+
+  isScrolledIntoView(el) {
+    let rect = el.getBoundingClientRect();
+    let elemTop = rect.top;
+    let elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    let isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
   }
 }
