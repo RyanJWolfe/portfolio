194bdcbc02a9f337b6c7f8f1d55df92dde2e4ceb
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-06-18 02:28:41 -0700

    Add render animations for navbar and home section

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,8 +13,35 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       transitionDuration: {
-        '0': '0ms',
-        '2000': '2000ms',
+        'nav-load': 'var(--nav-load-time)',
+        'nav-link-load': 'var(--nav-link-load-time)',
+      },
+      transitionDelay: {
+        'nav-load': 'var(--nav-load-time)',
+        'nav-link-load': 'var(--nav-link-load-time)',
+        'content-load': 'var(--content-load-delay)',
+      },
+      keyframes: {
+        'nav-load': {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        'nav-link-load': {
+          '0%': { transform: 'translateY(-25%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        'content-load': {
+          '0%': { transform: 'translateY(20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0px)', opacity: '1' },
+        },
+      },
+      animation: {
+        'nav-load': 'nav-load var(--nav-load-time) ease-in',
+        'nav-link-load': 'nav-link-load var(--nav-link-load-time) ease-in forwards',
+        'content-load': 'content-load 300ms ease-in var(--content-load-delay) forwards',
+        'content-load-long': 'content-load 300ms ease-in var(--content-long-load-delay) forwards',
+        'content-secondary-load': 'content-load 300ms ease-in var(--content-secondary-load-delay) forwards',
+        'content-secondary-load-long': 'content-load 300ms ease-in var(--content-secondary-long-load-delay) forwards',
       },
     },
   },
