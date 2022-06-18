72063a257828c04f2d1e8aa85626040d1e95c904
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-06-18 14:52:32 -0700

    Replace mobile menu dropdown with side menu

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -3,6 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["userDropdown", "dropdown"]
 
+  connect() {
+    window.addEventListener("click", (e) => {
+      if (e.target !== this.dropdownTarget)
+        this.hide()
+    })
+  }
+
   toggleUserMenu(e) {
     e.stopPropagation()
     this.hideMenuDropdown()
@@ -12,13 +19,17 @@ export default class extends Controller {
   toggleMenuDropdown(e) {
     e.stopPropagation()
     // this.hideUserDropdown()
-    this.dropdownTarget.classList.toggle('hidden')
+    if (this.dropdownTarget.style.left == '0') {
+      this.dropdownTarget.style.left = '-100%'
+    } else {
+      this.dropdownTarget.style.left = '0'
+    }
   }
 
   hide() {
     // this.hideUserDropdown()
     if (this.hasDropdownTarget)
-      this.dropdownTarget.classList.add('hidden')
+      this.dropdownTarget.style.left = '-100%'
   }
 
   hideUserDropdown() {
