7949545ae25893a6ba6db276559408ff8db6d169
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-06-20 15:43:28 -0700

    Use background color theme variable

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -32,9 +32,11 @@ export default class extends Controller {
 
   toggleTheme() {
     if (localStorage.theme === 'dark') {
+      document.documentElement.classList.remove('theme-dark')
       document.documentElement.classList.remove('dark')
       localStorage.theme = 'light'
     } else {
+      document.documentElement.classList.add('theme-dark')
       document.documentElement.classList.add('dark')
       localStorage.theme = 'dark'
     }
