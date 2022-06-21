eeb207519ae7120ddaecdaecc81fd0b5179c8540
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-06-20 22:34:50 -0700

    Add cool hamburger button that animates into side menu close btn

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,40 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["userDropdown", "dropdown"]
+  static targets = ["dropdown", "menuBtn"]
 
   connect() {
-    window.addEventListener("click", (e) => {
-      if (e.target !== this.dropdownTarget)
-        this.hide()
-    })
-  }
-
-  toggleUserMenu(e) {
-    e.stopPropagation()
-    this.hideMenuDropdown()
-    this.userDropdownTarget.classList.toggle('hidden')
+    this.sideMenuOpen = false
   }
 
   toggleMenuDropdown(e) {
     e.stopPropagation()
-    // this.hideUserDropdown()
-    if (this.dropdownTarget.style.right == '0') {
-      this.dropdownTarget.style.right = '-100%'
+    if (this.sideMenuOpen) {
+      this.hide()
     } else {
-      this.dropdownTarget.style.right = '0'
+      this.open()
     }
   }
 
   hide() {
-    // this.hideUserDropdown()
-    if (this.hasDropdownTarget)
+    if (this.hasDropdownTarget) {
+      this.menuBtnTarget.classList.remove('open')
       this.dropdownTarget.style.right = '-100%'
+      this.sideMenuOpen = false
+    }
   }
 
-  hideUserDropdown() {
-    if (this.hasUserDropdownTarget)
-      this.userDropdownTarget.classList.add('hidden')
+  open() {
+    this.menuBtnTarget.classList.add('open')
+    this.dropdownTarget.style.right = '0'
+    this.sideMenuOpen = true
   }
 
   hideMenuDropdown() {
