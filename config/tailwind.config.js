86ff388492d0494ca5d0191f33bd58d6d96f7a22
Ryan Wolfe <ryan.wolfe@strongmind.com>
2022-06-26 17:31:31 -0700

    Use montserrat as sans font, tweak about and hire layouts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
         poppins: 'Poppins',
       },
       height: {
