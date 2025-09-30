aeacf39e11f325cea4da084c217a43242e355746
Ryan Wolfe <xryanwolfe@gmail.com>
2025-09-29 17:47:52 -0700

    refactor: further dry up icon link rendering

---
+++ app/helpers/links_helper.rb
@@ -0,0 +1,10 @@
+module LinksHelper
+  def social_links
+    [
+      { name: "github", url: "https://github.com/RyanJWolfe" },
+      { name: "linkedin", url: "https://www.linkedin.com/in/ryan-wolfe-64652a115/" },
+      { name: "instagram", url: "https://www.instagram.com/theryanwolfe/" },
+      { name: "mail", url: "mailto:me@ryanjwolfe.com" }
+    ]
+  end
+end
