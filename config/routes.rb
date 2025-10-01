2957ef7b1fea75845849b919112ac4a8c8b08ceb
Ryan Wolfe <xryanwolfe@gmail.com>
2025-10-01 16:00:08 -0700

    feat: create blog controller with index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@
 
 Rails.application.routes.draw do
   root "pages#index"
+
+  get "/blog", to: "blog#index"
+  get "/blog/:slug", to: "blog#show", as: :blog_post
+
   get "/about", to: "pages#about"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#new"
-
-  # Contact form routes, not currently in use
   get "/contact", to: "contacts#new"
   get "/contact/success", to: "contacts#index"
   post "/contact", to: "contacts#create"
